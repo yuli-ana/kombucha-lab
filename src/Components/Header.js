4640ac0e9d71cb270c6c6f1f7e4ca7f5c0530505
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-19 01:21:14 -0400

    Create new folder Component and add 3 new components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+const Header = () => {
+    return (
+        <header>    
+            <h1>Kombucha Lab</h1>
+        </header>
+    )
+}
+
+export default Header;

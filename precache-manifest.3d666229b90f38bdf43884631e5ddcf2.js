e7a98844adb297e6971652c728a83d1dab501bd6
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-19 00:45:14 -0400

    Updates

---
+++ precache-manifest.3d666229b90f38bdf43884631e5ddcf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dce45c59f9e625a585586768b509c3a",
+    "url": "/yuli-ana/kombucha-lab.git/index.html"
+  },
+  {
+    "revision": "c951072cb2d69f8bbdfd",
+    "url": "/yuli-ana/kombucha-lab.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5cfdbab6e0bbef915dfa",
+    "url": "/yuli-ana/kombucha-lab.git/static/js/2.8a841b77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yuli-ana/kombucha-lab.git/static/js/2.8a841b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c951072cb2d69f8bbdfd",
+    "url": "/yuli-ana/kombucha-lab.git/static/js/main.81a5da26.chunk.js"
+  },
+  {
+    "revision": "a69ee0aa6ef607f1793f",
+    "url": "/yuli-ana/kombucha-lab.git/static/js/runtime-main.ac598121.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yuli-ana/kombucha-lab.git/static/media/logo.5d5d9eef.svg"
+  }
+]);

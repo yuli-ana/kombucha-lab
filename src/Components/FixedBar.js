5195db2cc65fe8334c5e388f71830a1417cbb1b2
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-21 13:19:38 -0400

    Added state to the component, updated state, added classes + event listeners

--- src/Components/FixedBar.js
+++ src/Components/FixedBar.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import Modal from './Modal';
+// import Button from './Button';
 
-const FixedBar = ({text}) => {
-    return (
+class FixedBar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            activeModal: false,
+        }
+    }
+
+    handleCartClick = () => {
+        this.setState({
+            activeModal: true,
+        })
+    }
+
+    handleClick = () => {
+        this.setState({
+            activeModal: false,
+        })
+    }
+
+    render() {
+        return (
             <div className='fixed-bar'>
-                <p>{text}</p>
+                <button onClick={this.handleCartClick} className='btn-cart'><FontAwesomeIcon className='icon' icon={faShoppingCart} /></button>
+                <div className={`modal ${this.state.activeModal ? 'openModal' : 'closeModal'}`}>
+                    <button onClick={this.handleClick} className='btn-close'><FontAwesomeIcon icon={faTimes}/></button>
+                    <ul className="sliding-menu">
+                        <li></li>
+                    </ul>
+                    <div>
+                        <p>total <span>$11.00</span></p>
+                        <a href='https://www.villagejuicery.com/collections/kombucha' target='blank' rel='noopener noreferer' >BUY</a>
+                    </div>
+                </div>
             </div>
-    )
+        )
+    }
 }
 
 export default FixedBar;
+
+
+
+
+
+

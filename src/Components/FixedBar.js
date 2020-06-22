1c394765dced62b74b2e2438bcc97d9a9534927e
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-22 13:13:34 -0400

    Used reduce method to count instances of selected items + chained map to render it in DOM

--- src/Components/FixedBar.js
+++ src/Components/FixedBar.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import React, { Component, Fragment } from 'react';
+import { faShoppingCart, faTimes, faMinusSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import Modal from './Modal';
 // import Button from './Button';
@@ -27,19 +27,48 @@ class FixedBar extends Component {
 
     render() {
         return (
-            <div className='fixed-bar'>
-                <button onClick={this.handleCartClick} className='btn-cart'><FontAwesomeIcon className='icon' icon={faShoppingCart} /></button>
-                <div className={`modal ${this.state.activeModal ? 'openModal' : 'closeModal'}`}>
-                    <button onClick={this.handleClick} className='btn-close'><FontAwesomeIcon icon={faTimes}/></button>
-                    <ul className="sliding-menu">
-                        <li></li>
-                    </ul>
-                    <div>
-                        <p>total <span>$11.00</span></p>
-                        <a href='https://www.villagejuicery.com/collections/kombucha' target='blank' rel='noopener noreferer' >BUY</a>
+            <Fragment>
+                <div className='fixed-bar'>
+                    <button onClick={this.handleCartClick} className='btn-cart'><FontAwesomeIcon className='icon' icon={faShoppingCart} />{this.props.totalCounter}</button>
+                    <div className={`modal ${this.state.activeModal ? 'openModal' : 'closeModal'}`}>
+                        <div className={this.state.activeModal ? 'bg' : 'bg-none'}></div>
+                        <button onClick={this.handleClick} className='btn-close'><FontAwesomeIcon icon={faTimes} /></button>
+                        <ul className="sliding-menu">
+                            {(this.props.items || [])
+                                .reduce((acc, value) => {
+
+                                    if (!acc.find(item => item.id === value.id)) {
+                                        acc.push(value);
+                                    }
+                                    return acc;
+
+                                }, [])
+                                .map((item, i) => {
+                                    console.log(this.props.count[item.id]);
+                                    return (
+                                        <li key={i} className='row cart-item'>
+                                            <div className='col-50'>
+                                                <img src={item.image} alt="name" />
+                                            </div>
+                                            <div className='cart-item-info col-50'>
+                                                <p>{item.name}</p>
+                                                <p>{item.price}</p>
+                                                <button className="btn-remove"><FontAwesomeIcon className='icon' icon={faMinusSquare} /></button>
+                                                <p>{this.props.count[item.id]}</p>
+                                            </div>
+                                        </li>
+                                    )
+                                })
+
+                            }
+                        </ul>
+                        <div>
+                            <p>total <span>$11.00</span></p>
+                            <a href='https://www.villagejuicery.com/collections/kombucha' target='blank' rel='noopener noreferer' >BUY</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fragment>
         )
     }
 }

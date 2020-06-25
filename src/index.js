6c0c724f492447e77be91fd54a94144463e7e864
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-25 01:24:15 -0400

    Add new font, import to the index.js + update styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Added new font
+import './fonts/Lato-Light.ttf'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
